f49eb8c9c6ed3182b76216d35c5eb84e1c0be32c
Guilherme Augusto <guiidiasdc@gmail.com>
2025-03-19 16:23:25 -0300

    formatação output usuario e adiçao do commander para CLI usage

--- src/cli.js
+++ src/cli.js
@@ -1,21 +1,48 @@
 import fs from "fs";
+import { Command } from "commander";
+import path from "path";
 import trataErros from "./erros/funcoesErro.js";
 import { contaPalavras } from "./index.js";
 import { montaSaidaArquivo } from "./helpers.js";
 
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
-const path = caminhoArquivo[3];
+const program = new Command();
 
-fs.readFile(link, "utf-8", (erro, texto) => {
-  try {
-    if (erro) throw erro;
-    const resultado = contaPalavras(texto);
-    criaESalvaArquivo(resultado, path);
-  } catch (erro) {
-    trataErros(erro);
-  }
-});
+program
+  .version("0.1")
+  .option("-t, --texto <string>", "⚙️ - Caminho do texto a ser processado")
+  .option("-d, --destino <string>", "⚙️ - Caminho para salvar resultados")
+  .action((options) => {
+    const { texto, destino } = options;
+    if (!texto || !destino) {
+      console.error("❌ - Erro inserir caminho de DESTINO e ORIGEM");
+      console.help();
+      return;
+    }
+
+    const caminhoTexto = path.resolve(texto);
+    const caminhoDestino = path.resolve(destino);
+
+    try {
+      processaArquivo(caminhoTexto, caminhoDestino);
+      console.log("✅ Texto Processado Com Sucesso ✅\n",);
+    } catch (error) {
+      console.log("❌ Ocorreu Erro No Processamento ❌", error);
+    }
+  });
+
+program.parse();
+
+function processaArquivo(texto, destino) {
+  fs.readFile(texto, "utf-8", (erro, texto) => {
+    try {
+      if (erro) throw erro;
+      const resultado = contaPalavras(texto);
+      criaESalvaArquivo(resultado, destino);
+    } catch (erro) {
+      trataErros(erro);
+    }
+  });
+}
 
 // * Função async usando método { .then .catch .finally }
 function criaESalvaArquivo(listaPalavras, path) {
@@ -24,10 +51,10 @@ function criaESalvaArquivo(listaPalavras, path) {
   fs.promises
     .writeFile(arquivoNovo, textoPalavras)
     .then(() => {
-      console.log("✅ Arquivo criado com sucesso");
+      console.log("✅ Arquivo criado com sucesso ✅\n");
     })
     .catch((error) => {
       throw error;
     })
-    .finally(() => console.log("✅ Operação finalizada com sucesso"));
+    .finally(() => console.log("✅ Operação finalizada com sucesso ✅\n"));
 }
